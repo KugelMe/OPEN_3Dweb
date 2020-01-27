ae96e720e6d402b0b740d0bd7d82c4db6800ee5b
Graham <graham.gibbons@gmail.com>
2020-01-27 09:32:32 -0800

    Use hash instead of search

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import models from "./*.glb";
 import view from "./view";
 
-const active = window.location.search.slice(1);
+const active = window.location.hash.substring(1);
 
 if (active && models[active]) {
   view(models[active]);
 } else {
   Object.keys(models).forEach(model => {
-    document.body.innerHTML += `<a href="/?${model}">${model}</a><br />`;
+    document.body.innerHTML += `<a href="/#${model}">${model}</a><br />`;
   });
 }
+
+window.addEventListener("hashchange", () => window.location.reload());
